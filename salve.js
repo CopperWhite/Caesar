7da654d76dce4b4c9ca4d020e180898fb78c4156
Alex <alextimkin@mail.ru>
2016-04-03 18:14:54 +0700

    fist commit

---
+++ salve.js
@@ -0,0 +1,21 @@
+$(function(){
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	$('#start-button').on('click', function () {
+		var result = [];
+		var word = $('#text-content').val().toLowerCase().split('');
+		var shift = Number($('#shift').val());
+		for (var i = 0; i <word.length; i++) {
+			for (var j = 0; j<26; j++) {
+				if (word[i] == alphabet[j]) {
+					if (j+shift>26) {
+						var letter = alphabet[j+shift-26]
+					} else {
+						var letter = alphabet[j+shift];
+					}
+					alert(letter);
+					$('#result-container').append(letter);
+				}
+			}
+		}
+	});
+});
